15ecdde5ec5f29653cbc99fec040b4061dadfff1
panagiotagyft <gyftoupanagiota@gmail.com>
2025-05-13 21:31:22 +0300

    comments

--- exercise_1/include/ex1_Serial.h
+++ exercise_1/include/ex1_Serial.h
@@ -3,6 +3,8 @@
 
 #include <stdio.h>
 
+// Performs a serial Monte Carlo simulation by throwing 'num_darts' random darts
+// into a unit square and counting how many land inside the unit circle.
 int serial(long long int);
 
 #endif // EX1_SERIAL_H
--- exercise_1/modules/ex1_Serial.c
+++ exercise_1/modules/ex1_Serial.c
@@ -8,29 +8,31 @@ int serial(long long int num_darts)
     long long int arrows;
     double x, y, squared_dist, total_time, start, end;
 
-    arrows = 0;
+    arrows = 0; // Initialize hit counter
+    
     start = ((double)clock()) / CLOCKS_PER_SEC;
+    
+    // Loop over the total number of dart throws
     for (long long int throw = 0; throw < num_darts; throw ++)
     {
-        // printf("Thread %d of %d\n", omp_get_thread_num(), omp_get_num_threads());
-        // printf("throw: %lld\n", throw);
+        // Generate a seed value based on current time and iteration index
+        // This helps diversify the random sequences between iterations
+        unsigned int seed = (unsigned int)time(NULL) ^ throw;
 
-        unsigned int seed = (unsigned int)time(NULL) ^ throw; // απλός seed για αποσυμφόρηση
-        x = (double)rand_r(&seed) / RAND_MAX * 2.0 - 1.0;     // float in range -1 to 1
+        // Generate a random x-coordinate in the range [-1.0, 1.0]
+        x = (double)rand_r(&seed) / RAND_MAX * 2.0 - 1.0;     
         y = (double)rand_r(&seed) / RAND_MAX * 2.0 - 1.0;
-        // printf("x: %f, y: %f\n", x, y);
 
+        // Compute the squared distance from the origin (0,0)
         squared_dist = (x * x) + (y * y);
 
+        // If the point lies within the unit circle, count it as a hit
         if (squared_dist <= 1)
             arrows++;
     }
     end = ((double)clock()) / CLOCKS_PER_SEC;
     total_time = end - start;
 
-    // printf("Arrows: %lld\n", arrows);
-    // printf("Darts: %lld\n", num_darts);
-    // printf("Serial process takes %f seconds to execute\n", total_time);
     printf("Serial,1,%lld,%lld,%.6f\n", num_darts, arrows, total_time);
 
     return EXIT_SUCCESS;
