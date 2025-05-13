21fb973f641db65b3f7a38a1c59c88c708c502cb
panagiotagyft <gyftoupanagiota@gmail.com>
2025-05-14 01:14:14 +0300

    comments

--- exercise_1/experiments.sh
+++ exercise_1/experiments.sh
@@ -1,29 +1,36 @@
 #!/bin/bash
 
-# Μεταγλώττιση
+# Compile the project
 make
 
-# Καθαρισμός φακέλου plots
+# Remove any existing plots directory
 rm -rf plots
 
-# Εκτελέσιμο πρόγραμμα
+# Path to the executable
 EXEC=./src/my_program
 
+# Output CSV file for results
 OUTFILE="results.csv"
 
-echo "Implementation,threads,num_darts,arrows,time" > "$OUTFILE"
+# Write CSV header
+echo "Implementation,threads,num_darts,arrows,time,pi" > "$OUTFILE"
 echo
 
-# Βρόχος για n = 10^4 έως 10^5
+# Loop over number of darts from 100 million to 1 billion, in steps of 100 million
 for ((n=100000000; n<=1000000000; n+=100000000)); do
 
-    for threads in 2 4 6 8; do
+    # For each dart count, run with different thread counts
+    for threads in 1 2 4 6 8; do
         echo "Running darts=$n threads=$threads"
+        # Execute the program and capture its output line
         line=$($EXEC -d $n -t $threads)
+        # Append the output to the CSV file
         echo "$line" >> "$OUTFILE"
     done
 done
 
+# Plot the results using the Python script
 python3 plot_results.py "$OUTFILE"
 
+# Clean build artifacts
 make clean
