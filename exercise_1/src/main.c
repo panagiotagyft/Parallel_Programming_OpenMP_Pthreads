a276ac85f33ac3a9910ab98f2f262a73f38b0a6d
panagiotagyft <gyftoupanagiota@gmail.com>
2025-05-13 21:33:43 +0300

    comments

--- exercise_1/src/main.c
+++ exercise_1/src/main.c
@@ -12,11 +12,21 @@ int main(int argc, char *argv[])
 
     srand(10);
 
+    // Parse command-line arguments and set num_darts and thread_count accordingly
     config(argc, argv, &num_darts, &thread_count);
 
-    serial(num_darts);
-    open_mp(num_darts, thread_count);
-    pthreads_impl(num_darts, thread_count);
+    if (thread_count == 1)
+    {
+        serial(num_darts); // If only one thread is requested, run the serial implementation
+    }
+    else
+    {
+        // Otherwise, run the parallel implementations in two ways:
+        // 1) Using OpenMP
+        open_mp(num_darts, thread_count);
 
+        // 2) Using Pthreads
+        pthreads_impl(num_darts, thread_count);
+    }
     return EXIT_SUCCESS;
 }
